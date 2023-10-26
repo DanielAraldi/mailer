47d5377cafbf08d5ee906eb26f100dd55745b4e6
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-10-26 15:03:48 -0300

    feat: ensure `EmailValidation` class validate email

---
+++ src/infra/validators/email-validator-adapter.ts
@@ -0,0 +1,9 @@
+import validator from 'validator';
+
+import { EmailValidator } from '../../validation/protocols';
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid(email: string): boolean {
+    return validator.isEmail(email);
+  }
+}
--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,2 +1,3 @@
+export * from './invalid-param-error';
 export * from './send-failed-error';
 export * from './server-error';
