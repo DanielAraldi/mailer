b51be40268f08f33b2505a7f4cda8ef07d519cf9
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-01-04 16:07:29 -0300

    feat: created `MultiEmailsInTheFieldValidation` to validate emails in the field

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,4 +1,5 @@
 export * from './email-validation';
+export * from './multi-emails-in-the-field-validation';
 export * from './required-field-validation';
-export * from './required-field-values-validations';
+export * from './required-multi-values-in-the-field-validations';
 export * from './validation-composite';
