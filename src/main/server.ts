0b9aa83f087822b6e8c0f7ba88e35ae518e0c338
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-01-04 16:59:55 -0300

    fix: small adjusts in path to start application

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,13 +4,8 @@ import { PrismaHelper } from '../infra/db';
 
 PrismaHelper.connect()
   .then(() => {
-    if (!PrismaHelper.isConnected) {
-      app
-        .listen({ port: env.port, host: '0.0.0.0' })
-        .then(() =>
-          console.log(`Server running in http://localhost:${env.port}`)
-        )
-        .catch(console.error);
+    if (PrismaHelper.isConnected) {
+      app.listen({ port: env.port, host: '0.0.0.0' }).catch(console.error);
     } else {
       console.error('Server not running! Please connect with database.');
     }
