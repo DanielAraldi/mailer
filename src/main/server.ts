84d07d7d83a92e41f31861414fa4b5035700c5b8
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-01-04 16:20:53 -0300

    feat: added connect prisma in server file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,18 @@
 import app from './config/app';
 import env from './config/env';
+import { PrismaHelper } from '../infra/db';
 
-app
-  .listen({ port: env.port, host: '0.0.0.0' })
-  .then(() => console.log(`Server running in http://localhost:${env.port}`))
+PrismaHelper.connect()
+  .then(() => {
+    if (PrismaHelper.isConnected) {
+      app
+        .listen({ port: env.port, host: '0.0.0.0' })
+        .then(() =>
+          console.log(`Server running in http://localhost:${env.port}`)
+        )
+        .catch(console.error);
+    } else {
+      console.error('Server not running! Please connect with database.');
+    }
+  })
   .catch(console.error);
