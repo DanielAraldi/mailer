cdad8a46e2e0ff6cdf2ca88a272113d6fd94936c
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-01-04 12:46:18 -0300

    temp: small adjusts

--- tests/infra/db/prisma/send-prisma-repository.spec.ts
+++ tests/infra/db/prisma/send-prisma-repository.spec.ts
@@ -1,22 +1,19 @@
+import { PrismaClient } from '@prisma/client';
 import { PrismaHelper, SendPrismaRepository } from '../../../../src/infra/db';
 import { SendEmailRepository } from '../../../../src/data/protocols/db';
 import { mockSendMailParams } from '../../../../tests/domain/mocks';
 
-let mailTable;
+let mailTable: PrismaClient['mail'];
 
 const makeSut = (): SendEmailRepository => new SendPrismaRepository();
 
 describe('SendPrismaRepository', () => {
-  beforeAll(async () => {
-    await PrismaHelper.connect();
-  });
+  beforeAll(async () => await PrismaHelper.connect());
 
-  afterAll(async () => {
-    await PrismaHelper.disconnect();
-  });
+  afterAll(async () => await PrismaHelper.disconnect());
 
   beforeEach(async () => {
-    mailTable = await PrismaHelper.getTable('Mail');
+    mailTable = await PrismaHelper.getTable('mail');
     await mailTable.deleteMany();
   });
 
@@ -25,7 +22,6 @@ describe('SendPrismaRepository', () => {
       const sut = makeSut();
       const sendMailParams = mockSendMailParams();
       const isValid = await sut.send(sendMailParams);
-
       expect(isValid).toBe(true);
     });
   });
