391d0ea5a207a5e00869a61163b3f73a5b2ac390
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-01-06 16:39:19 -0300

    feat: created html body from email scope

--- src/infra/mail/nodemailer-adapter.ts
+++ src/infra/mail/nodemailer-adapter.ts
@@ -29,11 +29,17 @@ export class NodemailerAdapter implements SendMail, Transporter {
 
     const { from, to, message, title, username } = mail;
     const hasUsername = username && !String(username).trim();
+    const subject = hasUsername ? `${title} - ${username}` : title;
     return await this.#transporter.sendMail({
       from,
       to,
-      text: message,
-      subject: hasUsername ? `${title} - ${username}` : title,
+      subject,
+      html: [
+        '<div style="min-height: 100%; font-family: sans-serif; background-color: #f5f5f5; color: #111; padding: 20px;">',
+        `<p style="font-size: 20px; font-weight: 500; mso-line-height-rule: exactly; text-align: center; margin-bottom: 16px;">${title}</p>`,
+        `<p style="font-size: 16px;">${message}</p>`,
+        '</div>',
+      ].join('\n'),
     });
   }
 }
