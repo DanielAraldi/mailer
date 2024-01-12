342de17342646fc2177403225cc23d76c3120837
RafaelMotaAlvess <leafardzn@gmail.com>
2024-01-12 12:28:57 -0300

    feat: typing the schema parameter

--- prisma/jest-environment-prisma/generate-database-url.ts
+++ prisma/jest-environment-prisma/generate-database-url.ts
@@ -1,4 +1,4 @@
-export const generateDatabaseUrl = (schema): string => {
+export const generateDatabaseUrl = (schema: string): string => {
   if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not set");
   }
