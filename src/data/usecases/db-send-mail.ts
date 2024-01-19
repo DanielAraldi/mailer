d73bc4893f291645fff76b6262aebf9b77c15eb3
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-01-19 18:18:53 -0300

    feat: should return false if sendMail() from `NodemailerAdapter` fails

--- src/data/usecases/db-send-mail.ts
+++ src/data/usecases/db-send-mail.ts
@@ -1,14 +1,17 @@
 import { SendEmail } from '../../domain';
 import { SendMail, Transporter } from '../protocols/adapters';
+import { SendEmailRepository } from '../protocols/db';
 
 export class DbSendMail implements SendEmail {
   constructor(
     private readonly transporter: Transporter,
-    private readonly sendMail: SendMail
+    private readonly sendMail: SendMail,
+    private readonly sendPrismaRepository: SendEmailRepository
   ) {}
 
   async send(mail: SendEmail.Params): Promise<SendEmail.Result> {
     this.transporter.create(mail);
-    return await this.sendMail.send(mail);
+    const wasSent = await this.sendMail.send(mail);
+    return wasSent ? await this.sendPrismaRepository.send(mail) : false;
   }
 }
--- src/main/factories/usecases/db-send-mail.ts
+++ src/main/factories/usecases/db-send-mail.ts
@@ -1,7 +1,13 @@
 import { DbSendMail } from '../../../data/usecases';
+import { SendPrismaRepository } from '../../../infra/db';
 import { NodemailerAdapter } from '../../../infra/mail';
 
 export const makeDbSendMail = (): DbSendMail => {
+  const sendPrismaRepository = new SendPrismaRepository();
   const nodemailerAdapter = new NodemailerAdapter();
-  return new DbSendMail(nodemailerAdapter, nodemailerAdapter);
+  return new DbSendMail(
+    nodemailerAdapter,
+    nodemailerAdapter,
+    sendPrismaRepository
+  );
 };
